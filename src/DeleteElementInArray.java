03bcb327af36c2fba2956cd2b9046e426bc03bf0
Anivarya Saini <anivarya590@gmail.com>
2026-01-28 21:33:11 +0530

    Refactor DeleteElementInArray class

--- src/DeleteElementInArray.java
+++ src/DeleteElementInArray.java
@@ -4,37 +4,27 @@ public class DeleteElementInArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Array Element Deleter");
-
-        // 1. Array Input liya
         int[] numArr = ArrayUtility.inputArray();
-
         System.out.print("Enter the number you want to delete: ");
         int numberToDelete = input.nextInt();
-
-        // 2. Naya Array banaya
         int[] newArr = deleteNumber(numArr, numberToDelete);
 
         System.out.println("Here is your new array:");
-        // Print karne ke liye loop ya Arrays.toString use karein
         for(int num : newArr){
             System.out.print(num + " ");
         }
     }
 
     public static int[] deleteNumber(int[] numArr, int numberToDelete) {
-        // Step 1: Occurrences count karo (Tumhara purana code reuse kar sakte ho!)
-        int occ = OccurrencesArray.noOfOccurrences(numArr, numberToDelete); // Agar class public hai to call kar lo
+        int occ = OccurrencesArray.noOfOccurrences(numArr, numberToDelete); 
 
-        // Ya manually count kar lo agar wo file link nahi hai
         if (occ == 0) {
-            return numArr; // Agar number hai hi nahi, to wahi wapas kar do
+            return numArr; 
         }
 
-        // Step 2: Naya size
         int newSize = numArr.length - occ;
         int[] newArr = new int[newSize];
 
-        // Step 3: Copy only non-target elements
         int i = 0, j = 0;
         while (i < numArr.length) {
             if (numArr[i] != numberToDelete) {
@@ -45,4 +35,5 @@ public class DeleteElementInArray {
         }
         return newArr;
     }
+
 }
