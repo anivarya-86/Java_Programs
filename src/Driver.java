479f0f9acca20813e4b086f6ef4799b98f9199ba
Anivarya Saini <anivarya590@gmail.com>
2026-02-11 22:39:22 +0530

    Add files via upload

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.drive();
+        myCar.addFuel(9);
+        myCar.drive();
+        myCar.getCurrentFuel();
+        myCar.color();
+        myCar.speed();
+
+    }
+}
