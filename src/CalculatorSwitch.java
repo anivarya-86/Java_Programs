1c4936405dcdd08459b29bc2a0fdb80c5d172a6f
Anivarya Saini <anivarya590@gmail.com>
2026-03-01 22:20:23 +0530

    Add files via upload

--- src/CalculatorSwitch.java
+++ src/CalculatorSwitch.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
 public class CalculatorSwitch {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Mini Calculator ");
+        System.out.print("Please enter the number : ");
+       double firstNum = input.nextDouble();
+        System.out.print("Again enter number :");
+        double secondNum =input.nextDouble();
+        System.out.println("Please Enter the Operator : (+ , - , * , / , % )");
+        char operator = input.next().charAt(0);
+        double result;
+
+
+
+        switch (operator){
+            case '+':
+                result = firstNum + secondNum;
+                System.out.println("Result = " + result);
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                System.out.println("Result = " + result);
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                System.out.println("Result = " + result);
+                break;
+            case '/':
+                result = firstNum / secondNum;
+                System.out.println("Result = " + result);
+                break;
+            case '%':
+                result = firstNum % secondNum;
+                System.out.println("Result = " + result);
+                break;
+        }
+
+
+    }
+
 }
