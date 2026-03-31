58fa6a5ff821d23df56e98e3f252ebfa983fa78e
Anivarya Saini <anivarya590@gmail.com>
2026-03-31 21:24:41 +0530

    Add files via upload

---
+++ in/Shape.java
@@ -0,0 +1,6 @@
+package in.abstraction;
+
+public abstract  class Shape {
+
+    public abstract double calculateArea();
+}
---
+++ in/TestShape.java
@@ -0,0 +1,15 @@
+package in.abstraction;
+
+
+public class TestShape {
+         public static void main(String[] args) {
+             
+         
+    Circle circle = new Circle(4);
+
+    Square square = new Square(5);
+
+    System.out.println("Your Circle Radius is " + circle);
+             System.out.println("Your Square SideIn Cm in " + square + "cm");
+}
+}
