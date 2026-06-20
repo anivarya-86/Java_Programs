727afd6b67277071c042e9606f86ed95341d195d
Anivarya Saini <anivarya590@gmail.com>
2026-06-20 00:34:08 -0700

    Add files via upload

---
+++ in/TestEnums.java
@@ -0,0 +1,12 @@
+package again;
+
+public class TestEnums {
+
+    public static void main(String[] args) {
+        System.out.println("Printing All the Days : ");
+        for( DayEnums dayEnums:DayEnums.values()){
+            System.out.println(dayEnums);
+        }
+    }
+
+}
