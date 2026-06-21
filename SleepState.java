d66b46c6c72d0eba30c1d703391001beaa9b459e
Anivarya Saini <anivarya590@gmail.com>
2026-06-21 01:23:36 -0700

    Add files via upload

---
+++ SleepState.java
@@ -0,0 +1,9 @@
+package TopProgram;
+
+public class SleepState  {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Before Sleeping");
+        Thread.sleep(2000);
+        System.out.println("After Sleeping");
+    }
+}
