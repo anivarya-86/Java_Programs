f33a660777778b0d8ff3fa97c869f8819a2fbadc
Anivarya Saini <anivarya590@gmail.com>
2026-06-24 00:23:47 -0700

    Add files via upload

---
+++ src/TestingFactorial.java
@@ -0,0 +1,32 @@
+package MostPrograms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class TestingFactorial {
+    public static void main(String[] args) {
+        try(ExecutorService service = Executors.newFixedThreadPool(3)) {
+            List<Future<Integer>> list = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                FactorialCalculator task = new FactorialCalculator(i);
+               list.add(service.submit(task));
+
+            }
+            for (Future<Integer> future : list){
+                System.out.printf("\n Result is : %d " , future.get());
+            }
+          //  service.shutdown();
+
+            if (!service.awaitTermination(10, TimeUnit.SECONDS)){
+                System.out.println("Bhout hua Stop......");
+                service.shutdown();
+            }
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
