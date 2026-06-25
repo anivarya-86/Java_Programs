fcccde9cb9e57c9675e8c19a772dd126098108aa
Anivarya Saini <anivarya590@gmail.com>
2026-06-24 23:57:54 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,20 @@
+package FunctionalProgram;
+
+public class Employee {
+
+    private final String  name;
+    private final int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
